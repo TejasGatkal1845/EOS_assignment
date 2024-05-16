e83c95bae593660c344651b60415bae7c5a09fb3
Tejasgatkal <gatkaltejas@gmail.com>
2024-05-16 18:27:32 +0530

    day11

---
+++ day11/que1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+int main() {
+	int ret, err, s, fd;
+	printf("parent started!\n");
+	ret = fork();
+	if(ret == 0) {
+		// output redirection
+		fd = open("in.txt", O_RDONLY);
+		close(0); // stdin
+		dup(fd); // copy fd on stdin
+		close(fd);
+
+		err = execlp("wc", "wc", NULL);
+		if(err < 0) {
+			perror("exec() failed");
+			_exit(1);
+		}
+	}
+	else
+		waitpid(-1, &s, 0);
+	printf("parent completed!\n");
+	return 0;
+}
+
