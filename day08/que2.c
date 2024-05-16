2dfed32db6b91fbc905eb7ff5706e425c0b28d5f
Tejasgatkal <gatkaltejas@gmail.com>
2024-05-16 14:33:03 +0530

    day09

--- day08/que2.c
+++ day08/que2.c
@@ -5,14 +5,16 @@
 
 int main()
 {
-	int ret1,ret2,ret3,ret4,ret5,S1,S2,S3,S4,S5;
+	int ret1,ret2,ret3,ret4,ret5,S1,S2,S3,S4,S5,i=0;
 
 	ret1 = fork();
 	if(ret1 == 0)
-	{
+	{printf("trjk\n");
+	
 		ret2 =fork();
 		if(ret2 == 0)
-		{
+		{ printf("fgerr\n");
+	
 			ret3 =fork();
 			if(ret3 == 0)
 			{
@@ -24,56 +26,59 @@ int main()
 					{
 						for(int i =0;i<5;i++)
 						{
-							printf("5th process is %d and pid is %d",i,getpid());
+							printf("5th process is %d and pid is %d\n",i,getpid());
 							sleep(1);
 						}
-						waitpid(ret5,&S5,0);
-						printf("child exit status id %d",WEXITSTATUS(S5));
-
+				//		waitpid(ret5,&S5,0);
+						printf("child exit status id %d\n",WEXITSTATUS(S5));
+						_exit(0);
 					}
-					_exit(0);
+				
 					for(int i =0;i<5;i++)
 					{
-						printf("4th process is %d and pid is %d",i,getpid());
+						printf("4th process is %d and pid is %d\n",i,getpid());
 						sleep(1);
 					}
-					waitpid(ret4,&S4,0);
-					printf("child exit status id %d",WEXITSTATUS(S4));
+				//waitpid(ret4,&S4,0);
 
+					waitpid(ret5,&S5,0);
+					printf("child exit status id %d\n",WEXITSTATUS(S4));
+					_exit(0);
 				}
-				_exit(0);
+				
 
 				for(int i =0;i<5;i++)
 				{
-					printf("3rd process is %d and pid is %d",i,getpid());
+					printf("3rd process is %d and pid is %d\n",i,getpid());
 					sleep(1);
 				}
-				waitpid(ret3,&S3,0);
-				printf("child exit status id %d",WEXITSTATUS(S3));
-
+				waitpid(ret4,&S4,0);
+				printf("child exit status id %d\n",WEXITSTATUS(S3));
+				_exit(0);
 			}
-			_exit(0);
+
+			
 
 			for(int i =0;i<5;i++)
 			{
-				printf("2nd process is %d and pid is %d",i,getpid());
+				printf("2nd process is %d and pid is %d\n",i,getpid());
 				sleep(1);
 			}
-			waitpid(ret2,&S2,0);
-			printf("child exit status id %d",WEXITSTATUS(S2));
-
+			waitpid(ret3,&S3,0);
+			printf("child exit status id %d\n",WEXITSTATUS(S2));
+			_exit(0);
 		}
-		_exit(0);
+		
 		for(int i =0;i<5;i++)
 		{
-			printf("1st process is %d and pid is %d",i,getpid());
+			printf("1st process is %d and pid is %d\n",i,getpid());
 			sleep(1);
 		}
-		waitpid(ret1,&S1,0);
-		printf("child exit status id %d",WEXITSTATUS(S1));
-
+		waitpid(ret2,&S2,0);
+		printf("child exit status id %d\n",WEXITSTATUS(S1));
+		_exit(0);
 	}
-	_exit(0);
+	waitpid(ret1,&S1,0);
 
 	return 0;
 }
